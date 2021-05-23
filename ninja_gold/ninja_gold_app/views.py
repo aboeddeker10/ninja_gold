d3aab2f31e96f9aeab0db9fe0d025eb729faa5e1
Andrew Boeddeker <andrewboeddeker@gmail.com>
2021-05-23 08:52:54 -0500

    commit of updated files

--- ninja_gold/ninja_gold_app/views.py
+++ ninja_gold/ninja_gold_app/views.py
@@ -1,8 +1,28 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+import random
 
 # Create your views here.
+GOLD_MAP = {
+    "farm": (10, 20),
+    "cave": (5, 10),
+    "house": (2, 5),
+    "casino": (0, 50)
+}
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+yourgold = 0
+
+
+def process(request):
+    if 'name' == 'farm':
+        request.session['farm'] = request.POST['farm']
+        yourgold += random(10-20)
+
+
+def gold(request):
+    pass
